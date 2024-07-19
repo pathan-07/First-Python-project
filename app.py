5b340eab3900741b0d8a3e87f3c784e13046ed52
Pathan Mo. Faizan Khan <132596404+pathan-07@users.noreply.github.com>
2024-07-19 13:11:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from Scikits Learn.linear_model import LogisticRegression
+from Scikits Learn.preprocessing import StandardScaler
+from Scikits Learn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
